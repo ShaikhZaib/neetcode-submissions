ea832824b4aae863f4338ec672e9832bae832b6f
Zaib <88584755+ShaikhZaib@users.noreply.github.com>
2026-07-05 12:18:59 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<String, Set<Character>> squares = new HashMap<>();
+
+        for(int row = 0; row < 9; row++){
+            for(int col = 0; col < 9; col++){
+                char currChar = board[row][col];
+                if(currChar == '.') continue;
+
+                String squareKey = (row / 3) + "," + (col / 3);
+
+                if(rows.computeIfAbsent(row, k -> new HashSet<>()).contains(currChar) ||
+                   cols.computeIfAbsent(col, k -> new HashSet<>()).contains(currChar) ||
+                   squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(currChar)
+                ){
+                    return false;
+                }
+
+                rows.get(row).add(currChar);
+                cols.get(col).add(currChar);
+                squares.get(squareKey).add(currChar);
+            }
+        }
+
+        return true;
+    }
+    // Time Complexity -> O(n²)
+    // Space Complexity -> O(n²)
+}
